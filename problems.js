a5318a673bcb53a75846fbf3b2aa20a062e8c041
WTDula <wtdula@gmail.com>
2022-07-16 13:48:12 -0500

    solved romanToInt, tested

--- problems.js
+++ problems.js
@@ -113,5 +113,21 @@ console.log(twoSum(nums, 9))
  * @return {number}
  */
  var romanToInt = function(s) {
-    
+    let obj = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+      }
+      let total = 0
+      for(let i = 0; i < s.length; i++){
+        obj[s[i]]< obj[s[i+1]]? total-=obj[s[i]] : total+=obj[s[i]]
+      }
+      return total
 }
+//test
+console.log("IV in numerals is: ", romanToInt('IV'))
+console.log("XIX in numerals is: ", romanToInt('XIX'))
